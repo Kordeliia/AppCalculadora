7b14b4369c8efb3615c4a04d367034ff1bedcabf
Kordeliia <lalvmun@hotmail.com>
2023-09-27 16:46:11 +0200

    commit solventar errores y excepciones

--- app/src/main/java/com/example/appcalculadora/MainActivity.kt
+++ app/src/main/java/com/example/appcalculadora/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import com.example.appcalculadora.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -48,10 +49,37 @@ class MainActivity : AppCompatActivity() {
     private fun tryResolve(operationRef: String) {
         val operator = getOperator(operationRef)
         var values = arrayOfNulls<String>(0)
-        values = operationRef.split(operator).toTypedArray()
-        val numberOne = values[0]!!.toDouble()
-        val numberTwo = values[1]!!.toDouble()
-        binding.tvReultado.text = getResult(numberOne, numberTwo, operator).toString()
+        if (operator != OPERATOR_NULL){
+            if(operator == OPERATOR_RESTA)
+            {
+                val index = operationRef.lastIndexOf(OPERATOR_RESTA)
+                if(index < operationRef.length-1){
+                    values = arrayOfNulls(2)
+                    values[0] = operationRef.substring(0, index)
+                    values[1] = operationRef.substring(index+1)
+                }
+                else{
+                    values = arrayOfNulls(1)
+                    values[0] = operationRef.substring(0, index)
+                }
+            } else{
+                values = operationRef.split(operator).toTypedArray()
+
+            }
+            if(values.size > 1){
+                try {
+                    val numberOne = values[0]!!.toDouble()
+                    val numberTwo = values[1]!!.toDouble()
+                    binding.tvReultado.text = getResult(numberOne, numberTwo, operator).toString()
+                } catch (e: NumberFormatException) {
+                    Snackbar.make(binding.root, "La expresion NO es correcta", Snackbar.LENGTH_SHORT).show()
+                }
+            } else{
+                Snackbar.make(binding.root, "La expresion NO es correcta", Snackbar.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 
     private fun getOperator(operation: String): String {
@@ -64,10 +92,14 @@ class MainActivity : AppCompatActivity() {
             operator = OPERATOR_MULTI
         } else if (operation.contains(OPERATOR_DIV)) {
             operator = OPERATOR_DIV
+        } else {
+            operator = OPERATOR_NULL
+        }
+        if (operator == OPERATOR_NULL && operation.lastIndexOf(OPERATOR_RESTA) > 0) {
+        operator = OPERATOR_RESTA
         }
         return operator
     }
-
     private fun getResult(numberOne: Double, numberTwo : Double, operator : String) : Double{
         var result = 0.0
         when(operator){
@@ -84,5 +116,6 @@ class MainActivity : AppCompatActivity() {
         const val OPERATOR_RESTA = "-"
         const val OPERATOR_DIV = "/"
         const val OPERATOR_DECIMAL = "."
+        const val OPERATOR_NULL = "null"
     }
 }
