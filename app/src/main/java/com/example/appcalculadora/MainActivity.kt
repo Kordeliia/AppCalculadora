9eb9e1275bda4e8970bd6b2e6d2927eab15da5a8
Kordeliia <lalvmun@hotmail.com>
2023-09-27 13:03:07 +0200

    commit añadir click desde xml

--- app/src/main/java/com/example/appcalculadora/MainActivity.kt
+++ app/src/main/java/com/example/appcalculadora/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.appcalculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.example.appcalculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun onClickButton(view : View){
+        Log.i("click", (view as Button).text.toString())
     }
 }
